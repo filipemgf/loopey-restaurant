ef90982841fcb622b2aa4d58e0b0f10ab81c7382
Filipe Ferreira <filipeferreira94@gmail.com>
2023-05-31 11:50:23 +0100

    Using handlebars to reuse code for each page with res.render and {{{body}}} on the layout

--- app.js
+++ app.js
@@ -11,12 +11,12 @@ app.set("view engine", "hbs"); //sets HBS as my template engine
 app.get("/", (request, response, next) => {
 	//req, res,next are objects that have info and functionality related to the request
 	console.log("homepage has been requested");
-	response.sendFile(__dirname + "/views/home-page.html");
+	response.render(__dirname + "/views/home-page.hbs");
 });
 
 app.get("/contact-page", (req, res, next) => {
 	console.log("contact page has been requested");
-	res.sendFile(__dirname + "/views/contact-page.html");
+	res.render(__dirname + "/views/contact-page.hbs"); //render instead of sendFile because we're rendering a view with Handlebars
 });
 
 const pizzaDetails = {
